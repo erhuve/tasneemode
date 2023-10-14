b76bda5c6a3556f50b65d1f6378bf8e4f2722ae0
Raymond <ray.luo@nyu.edu>
2023-10-14 18:33:05 -0400

    polaroids

---
+++ camera.js
@@ -0,0 +1,20 @@
+counter = 1;
+function takePic() {
+  $(`#p${counter++}`).show();
+}
+
+function flash(e) {
+  $(".flash")
+    .show() //show the hidden div
+    .animate({ opacity: 0.7 }, 300)
+    .fadeOut(300)
+    .css({ opacity: 1 });
+}
+
+$(document).ready(function () {
+  $(".flash").hide();
+  $(".item").hide();
+  $(".flashbutton").mouseup(function (e) {
+    flash(e);
+  });
+});
